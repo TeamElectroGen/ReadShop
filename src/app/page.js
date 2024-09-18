3c0ad29915ef638a94c5196e682f4f0fe3d9330b
Abdullah Al Maksud <maksud.workspace@gmail.com>
2024-09-18 20:03:03 +0600

    Add Prettier and Tailwind CSS class sorting configuration, Set FavIcon, Site Title and Site description.

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className="">Hi I am Nazrul Islam. What are you doing now?</div>;
+  return (
+    <div className="m-6 bg-green-200 text-xl">
+      Hi I am Nazrul Islam. What are you doing now?
+    </div>
+  );
 }
