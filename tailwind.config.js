17d2d38432af76e7291fc5000beb9af48b8f7183
Abdullah Al Maksud <maksud.workspace@gmail.com>
2024-09-19 09:01:34 +0600

    add: custom color and font for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,29 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // background: "var(--background)", // Default Background
+        background: "#f9fafb", // Light Background Color
+        foreground: "var(--foreground)", //Default Foreground Color
+        primary: "#401E8A", // Deep Violet
+        secondary: "#f97316", // Vivid Orange
+        accent: "#10b981", // Emerald G
+        lightGray: {
+          100: "#f9fafb", // Very Light Gray
+          200: "#f3f4f6", // Light Gray
+        },
+        mediumGray: {
+          500: "#6b7280", // Medium Gray
+        },
+        darkGray: {
+          800: "#1f2937", // Dark Gray
+        },
+      },
+      
+      fontFamily: {
+        serif: ["Merriweather", "serif"],
+        sans: ["Montserrat", "sans-serif"],
+        display: ["Shadows Into Light", "cursive"],
+        mono: ["Roboto Mono", "monospace"],
       },
     },
   },
