fbabb8165d52573fe8a18b73d5966372feb2efdf
imranhasan2 <imranhasan030303@gmail.com>
2024-09-19 09:28:21 +0600

    setup contuct us structure

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,9 +1,39 @@
+import Navbar from "@/components/Navbar";
 import React from "react";
 
 const ContactUs = () => {
   return (
-    <div>
-      <h1>Contact Us</h1>
+    <div className="mb-5">
+      <Navbar />
+      {/* there will be a backgorund image next ..youhave to add a bg image down */}
+      <div className=" bg-custom-gray h-[20vh]">
+        <img src="" alt="" />
+      </div>
+
+      {/*  */}
+      <div className="max-w-7xl h-full md:flex lg:flex  gap-4 mt-4 " >
+        <div className="w-1/2 ml-6 ">
+          <form className="flex flex-col gap-4  " >
+            <input className="p-2 border border-gray-300 w-full" type="text" name="name" required />
+            <input className="p-2 border border-gray-300" type="text" name="email" required />
+            <input className="p-2 border border-gray-300" type="text" name="phone" required />
+            <select className="p-2 border border-gray-300" name="issues" id="" required>
+              <option disabled selected>Select Subject</option>
+              <option value=" Order Related Issue "> Order Related Issue </option>
+              <option value=" Payment/Balance/Wallet/Refund "> Payment/Balance/Wallet/Refund </option>
+              <option value=" Delivery Related Issue "> Delivery Related Issue </option>
+              <option value=" Product-Related info "> Product-Related info </option>
+              <option value=" Product Info Missing/Mistake "> Product Info Missing/Mistake </option>
+            </select>
+            <textarea className="p-2 border border-gray-300" name="message" id="" cols={30} rows={7}></textarea>
+            <button className="bg-green-600 p-4 "style={{width:'100px'}} >Submit</button>
+          </form>
+        </div>
+        <div className="w-1/2">
+          {/* map will be add here */}
+          map will be add here
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'custom-gray': 'rgba(43, 43, 43, 0.7)',
       },
     },
   },
