52999c80da80d50e23dd0e8a37ecb01281dbb0fc
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-09-19 21:18:47 +0600

    Add: Our team section in about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,8 +12,8 @@ const About = () => {
           </div>
         </header>
 
-        <div className="mt-12 flex flex-col gap-4 border lg:flex-row lg:gap-6">
-          <div className="flex h-60 items-center justify-center border lg:w-1/2">
+        <div className="mt-12 flex flex-col gap-4 p-6 rounded-2xl bg-background lg:flex-row lg:gap-6">
+          <div className="flex bg-white items-center justify-center border lg:w-1/2">
             <Image alt="About image" />
           </div>
           <div className="flex-1">
@@ -38,6 +38,56 @@ const About = () => {
                 Let us be part of your reading adventure—wherever it may lead
                 you.
               </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-12 gap-4 p-6 bg-lightGray-200 rounded-2xl lg:flex-row lg:gap-6">
+          <h3 className="text-center font-semibold text-2xl">Our team</h3>
+        
+          {/* Team container */}
+          <div className="mt-8 grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-5">
+            {/* team card */}
+            <div className="h-52 p-2 w-40 rounded-t-full border-4">
+              <div className="h-3/4 flex justify-center items-center border rounded-full bg-white">
+                <Image alt="team member" />
+              </div>
+              <div className="bg-mediumGray-500 mt-2 h-10 w-full"></div>
+            </div>
+            {/* team card */}
+            <div className="h-52 p-2 w-40 rounded-t-full border-4">
+              <div className="h-3/4 flex justify-center items-center border rounded-full bg-white">
+                <Image alt="team member" />
+              </div>
+              <div className="bg-mediumGray-500 mt-2 h-10 w-full"></div>
+            </div>
+            {/* team card */}
+            <div className="h-52 p-2 w-40 rounded-t-full border-4">
+              <div className="h-3/4 flex justify-center items-center border rounded-full bg-white">
+                <Image alt="team member" />
+              </div>
+              <div className="bg-mediumGray-500 mt-2 h-10 w-full"></div>
+            </div>
+            {/* team card */}
+            <div className="h-52 p-2 w-40 rounded-t-full border-4">
+              <div className="h-3/4 flex justify-center items-center border rounded-full bg-white">
+                <Image alt="team member" />
+              </div>
+              <div className="bg-mediumGray-500 mt-2 h-10 w-full"></div>
+            </div>
+            {/* team card */}
+            <div className="h-52 p-2 w-40 rounded-t-full border-4">
+              <div className="h-3/4 flex justify-center items-center border rounded-full bg-white">
+                <Image alt="team member" />
+              </div>
+              <div className="bg-mediumGray-500 mt-2 h-10 w-full"></div>
+            </div>
+            {/* team card */}
+            <div className="h-52 p-2 w-40 rounded-t-full border-4">
+              <div className="h-3/4 flex justify-center items-center border rounded-full bg-white">
+                <Image alt="team member" />
+              </div>
+              <div className="bg-mediumGray-500 mt-2 h-10 w-full"></div>
             </div>
           </div>
         </div>
