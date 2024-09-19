b331c198ba628a6154a0f00e5be280d8d88d6a3f
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-09-19 11:52:43 +0600

    Add: header to about page, create: public folder

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
 import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <section>
+      <div className="container mx-auto px-4 2xl:px-36">
+        <header className="mt-12 flex h-40 items-center justify-center rounded-2xl bg-background">
+          <h1 className="text-3xl font-bold text-darkGray-800">About Us</h1>
+        </header>
+      </div>
+    </section>
   );
 };
 
