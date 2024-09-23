d5a7aecb4676f1100d83b0740f561c51b7e30f6b
masud4657 <masudpfrnt@gmail.com>
2024-09-23 16:31:58 +0600

    update:Update functionality of carousel arrow buttons in the popular books section on the homepage.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,14 @@ import Label from "@/components/Label";
 import React, { useEffect, useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+import { FaTimes } from "react-icons/fa";
 import Card from "@/components/Card";
 // import books from "@/data/books.json";
 
 
 const HomePage = () => {
   const [books, setBooks] = useState([]);
+  const [currentIndex,setCurrentIndex]= useState(0)
 
   useEffect(() => {
     // Fetching books.json from the public folder
@@ -20,6 +22,12 @@ const HomePage = () => {
 
     fetchBooks();
   }, []);
+  const handlePrev= ()=>{
+    setCurrentIndex((prevIndex)=>(prevIndex > 0 ? prevIndex - 1 : 0));
+  }
+  const handleNext=()=>{
+    setCurrentIndex((prevIndex)=>(prevIndex < books.length - 6 ? prevIndex + 1 : prevIndex));
+  }
   return (
     <div className="space-y-4">
       {/*Search & Filter Banner*/}
@@ -40,24 +48,24 @@ const HomePage = () => {
 
       {/*Popular books cards*/}
       <section className="relative container mx-auto  bg-[#d9d9d9] p-5">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-center">
           {/* label */}
           <Label name="Popular Books" />
           {/* pagination / carousel */}
           
-            <button className="absolute left-0 top-1/2 -translate-y-1/2 z-10 h-24 text-3xl bg-white p-2">
-              <FaAngleLeft />
+            <button onClick={handlePrev} disabled={currentIndex === 0}  className="absolute left-0 top-1/2 -translate-y-1/2 z-10 h-24 text-3xl bg-white p-2">
+            {currentIndex === 0 ? <FaTimes /> : <FaAngleLeft />}
             </button>
              
-            <button className="absolute right-0 top-1/2 -translate-y-1/2 z-10 h-24 text-3xl bg-white p-2">
-              <FaAngleRight />
+            <button onClick={handleNext} disabled={currentIndex >= books.length - 6} className="absolute right-0 top-1/2 -translate-y-1/2 z-10 h-24 text-3xl bg-white p-2">
+            {currentIndex >= books.length - 6 ? <FaTimes /> : <FaAngleRight />}
             </button>
             
          
         </div>
         {/* cards */}
-        <div className="mt-5 grid grid-cols-2 gap-5 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7">
-        {books.slice(0, 5).map((book, idx) => (
+        <div className="mt-5 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7">
+        {books.slice(currentIndex,currentIndex+6).map((book, idx) => (
             <Card key={idx} book={book} />
           ))}
         </div>
@@ -65,7 +73,7 @@ const HomePage = () => {
 
       {/*All books cards*/}
       <section className="relative container mx-auto  bg-[#d9d9d9] p-5">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-center">
           {/* label */}
           <Label name="Books" />
           {/* pagination / carousel */}
@@ -91,7 +99,7 @@ const HomePage = () => {
 
       {/*Another Section cards*/}
       <section className="relative container mx-auto  bg-[#d9d9d9] p-5">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-center">
           {/* label */}
           <Label name="Books" />
           {/* pagination / carousel */}
