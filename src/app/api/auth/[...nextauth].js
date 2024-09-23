9bb429681fb477055648b21680f54be7871db47a
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-09-23 20:57:27 +0600

    add: api route created for signin

---
+++ src/app/api/auth/[...nextauth].js
@@ -0,0 +1,48 @@
+import { connectDB } from "@/lib/connectDB";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import bcrypt from "bcrypt";
+
+const handler = NextAuth({
+  secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
+  session: {
+    strategy: "jwt",
+    maxAge: 60 * 60 * 24 * 30,
+  },
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        emailOrPhone: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        const { emailOrPhone, password } = credentials;
+        if (!emailOrPhone || !password) {
+          return null;
+        }
+        const db = await connectDB();
+        const currentUser = await db
+          .collection("users")
+          .findOne({ or: [{ email: emailOrPhone }, { phone: emailOrPhone }] });
+        if (!currentUser) {
+          return null;
+        }
+        const passwordMatched = bcrypt.compareSync(
+          password,
+          currentUser.password
+        );
+        if (!passwordMatched) {
+          return null;
+        }
+        return currentUser;
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
