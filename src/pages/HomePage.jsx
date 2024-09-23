3514b5d0d387b4009f2cc37a12d167f05ef05edb
masud4657 <masudpfrnt@gmail.com>
2024-09-23 11:51:20 +0600

    update:Update the carousel arrow button in the all books cards  section on the home page.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -64,19 +64,19 @@ const HomePage = () => {
       </section>
 
       {/*All books cards*/}
-      <section className="container mx-auto  bg-[#d9d9d9] p-5">
+      <section className="relative container mx-auto  bg-[#d9d9d9] p-5">
         <div className="flex items-center justify-between">
           {/* label */}
           <Label name="Books" />
           {/* pagination / carousel */}
-          <div className="flex items-center gap-2 *:bg-white *:p-2">
-            <button>
+          
+            <button className="absolute left-0 top-1/2 -translate-y-1/2 z-10 h-24 text-3xl bg-white p-2">
               <FaAngleLeft />
             </button>
-            <button>
+            <button className="absolute right-0 top-1/2 -translate-y-1/2 z-10 h-24 text-3xl bg-white p-2">
               <FaAngleRight />
             </button>
-          </div>
+          
         </div>
         {/* cards */}
         <div className="mt-5 grid grid-cols-2 gap-5 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7">
