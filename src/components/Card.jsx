efce89b3754dcb48118b48ee8f29feee649cd6e8
masud4657 <masudpfrnt@gmail.com>
2024-09-23 22:02:22 +0600

    add & update: added the view details button in card.jsx component and update the card design.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { Button } from "@/components/ui/button"
+
 
 
 const Card = ({ book }) => {
   return (
-    <div className="mx-auto w-[164px]">
+    <div className="mx-auto w-[164px] flex flex-col h-full">
      {/* Book Cover Image */}
      <div className="h-[229px] bg-[#f3f2f2]">
         <img src={book.CoverImage} alt={book.BookName} className="h-full w-full object-cover" />
         
       </div>
       {/* Book Info */}
+      <div className="flex flex-col flex-grow justify-between">
       <div className="mt-5 space-y-1">
         <p className="text-sm font-bold">{book.BookName}</p>
         <p className="text-sm"><span className="font-medium">AuthorName:</span> {book.AuthorName}</p>
@@ -19,6 +22,9 @@ const Card = ({ book }) => {
         <p className="text-sm "><span className="font-medium">Price:</span> {book.Price}</p>
         <p className="text-sm "><span className="font-medium">Rating:</span> {book.Rating}</p>
       </div>
+      <Button variant="outline">View Details</Button>
+      </div>
+
     </div>
   );
 };
