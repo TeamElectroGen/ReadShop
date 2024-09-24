d2f93c70c379b57a82448dbaa5bf7496836d2b31
masud4657 <masudpfrnt@gmail.com>
2024-09-24 15:55:17 +0600

    add: added the details page.

---
+++ src/pages/ViewDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ViewDetails = () => {
+    return (
+        <div>
+            <p className="text-red-700">view details page</p>
+        </div>
+    );
+}
+
+export default ViewDetails;
