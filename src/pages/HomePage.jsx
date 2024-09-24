05f4f0d9a9d16da51d860392ac1e7c17fd6276da
masud4657 <masudpfrnt@gmail.com>
2024-09-24 10:24:52 +0600

    update:home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,7 +10,11 @@ import Card from "@/components/Card";
 
 const HomePage = () => {
   const [books, setBooks] = useState([]);
-  const [currentIndex,setCurrentIndex]= useState(0)
+  // const [currentIndex,setCurrentIndex]= useState(0)
+  const [popularIndex,setPopularIndex]=useState(0)
+  const [allBooksIndex,setAllBooksIndex]=useState(0)
+  const [bestSellersIndex,setBestSellersIndex]=useState(0)
+  const [newPublishedIndex,setNewPublishedIndex]=useState(0)
 
   useEffect(() => {
     // Fetching books.json from the public folder
@@ -22,11 +26,39 @@ const HomePage = () => {
 
     fetchBooks();
   }, []);
-  const handlePrev= ()=>{
-    setCurrentIndex((prevIndex)=>(prevIndex > 0 ? prevIndex - 1 : 0));
+  // const handlePrev= ()=>{
+  //   setCurrentIndex((prevIndex)=>(prevIndex > 0 ? prevIndex - 1 : 0));
+  // }
+  // const handleNext=()=>{
+  //   setCurrentIndex((prevIndex)=>(prevIndex < books.length - 6 ? prevIndex + 1 : prevIndex));
+  // }
+  const handlePrev=(section)=>{
+    if(section==="popular"){
+      setPopularIndex((prevIndex)=>prevIndex>0?prevIndex-1:0)
+    }else if(section==="allBooks"){
+      setAllBooksIndex((prevIndex)=>prevIndex>0?prevIndex-1:0)
+    }else if(section==="bestSellers"){
+      setBestSellersIndex((prevIndex)=>prevIndex>0?prevIndex-1:0)
+    }else if(section==="newPublished"){
+      setNewPublishedIndex((prevIndex)=>prevIndex>0?prevIndex-1:0)
+    }
   }
-  const handleNext=()=>{
-    setCurrentIndex((prevIndex)=>(prevIndex < books.length - 6 ? prevIndex + 1 : prevIndex));
+  const handleNext=(section,length)=>{
+    if(section==="popular"){
+      setPopularIndex((prevIndex)=>prevIndex<length-6?prevIndex+1:prevIndex)
+    }else if(section==="allBooks"){
+      setAllBooksIndex((prevIndex) =>
+        prevIndex < length - 7 ? prevIndex + 1 : prevIndex
+      );
+    }else if (section === "bestSellers") {
+      setBestSellersIndex((prevIndex) =>
+        prevIndex < length - 7 ? prevIndex + 1 : prevIndex
+      );
+    } else if (section === "newPublished") {
+      setNewPublishedIndex((prevIndex) =>
+        prevIndex < length - 7 ? prevIndex + 1 : prevIndex
+      );
+    }
   }
   return (
     <div className="space-y-4">
@@ -47,25 +79,25 @@ const HomePage = () => {
      
 
       {/*Popular books cards*/}
-      <section className="relative container mx-auto  bg-[#d9d9d9] p-5">
+      <section className="relative container mx-auto bg-[#d9d9d9] p-5">
         <div className="flex items-center justify-center">
           {/* label */}
           <Label name="Popular Books" />
           {/* pagination / carousel */}
           
-            <button onClick={handlePrev} disabled={currentIndex === 0}  className="absolute left-0 top-1/2 -translate-y-1/2 z-10 h-24 text-3xl bg-white p-2">
-            {currentIndex === 0 ? <FaTimes /> : <FaAngleLeft />}
+            <button onClick={()=>handlePrev('popular')} disabled={popularIndex === 0}  className="absolute left-0 top-1/2 -translate-y-1/2 z-10 h-24 text-3xl bg-white p-2">
+            {popularIndex === 0 ? <FaTimes /> : <FaAngleLeft />}
             </button>
              
-            <button onClick={handleNext} disabled={currentIndex >= books.length - 6} className="absolute right-0 top-1/2 -translate-y-1/2 z-10 h-24 text-3xl bg-white p-2">
-            {currentIndex >= books.length - 6 ? <FaTimes /> : <FaAngleRight />}
+            <button onClick={()=>handleNext("popular",books.length)} disabled={popularIndex >= books.length-6} className="absolute right-0 top-1/2 -translate-y-1/2 z-10 h-24 text-3xl bg-white p-2">
+            {popularIndex >= books.length - 6 ? <FaTimes /> : <FaAngleRight />}
             </button>
             
          
         </div>
         {/* cards */}
-        <div className="mt-5 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7">
-        {books.slice(currentIndex,currentIndex+6).map((book, idx) => (
+        <div className="mt-5 grid justify-center items-center grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+        {books.slice(popularIndex,popularIndex+6).map((book, idx) => (
             <Card key={idx} book={book} />
           ))}
         </div>
@@ -78,17 +110,18 @@ const HomePage = () => {
           <Label name="Books" />
           {/* pagination / carousel */}
           
-            <button onClick={handlePrev} disabled={currentIndex === 0} className="absolute left-0 top-1/2 -translate-y-1/2 z-10 h-24 text-3xl bg-white p-2">
-            {currentIndex === 0 ? <FaTimes /> : <FaAngleLeft />}
+            <button onClick={()=>handlePrev("allBooks")} disabled={allBooksIndex === 0} className="absolute left-0 top-1/2 -translate-y-1/2 z-10 h-24 text-3xl bg-white p-2">
+            {allBooksIndex === 0 ? <FaTimes /> : <FaAngleLeft />}
             </button>
-            <button onClick={handleNext} disabled={currentIndex >= books.length - 6} className="absolute right-0 top-1/2 -translate-y-1/2 z-10 h-24 text-3xl bg-white p-2">
-            {currentIndex >= books.length - 7 ? <FaTimes /> : <FaAngleRight />}
+            <button onClick={() => handleNext("allBooks", books.length)}
+            disabled={allBooksIndex >= books.length - 7} className="absolute right-0 top-1/2 -translate-y-1/2 z-10 h-24 text-3xl bg-white p-2">
+            {allBooksIndex >= books.length - 7 ? <FaTimes /> : <FaAngleRight />}
             </button>
           
         </div>
         {/* cards */}
         <div className="mt-5 grid grid-cols-2 gap-5 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7">
-        {books.slice(currentIndex,currentIndex+7).map((book, idx) => (
+        {books.slice(allBooksIndex,allBooksIndex+7).map((book, idx) => (
             <Card key={idx} book={book} />
           ))}
         </div>
@@ -104,17 +137,19 @@ const HomePage = () => {
           <Label name="Best sellers Books" />
           {/* pagination / carousel */}
           
-            <button onClick={handlePrev} disabled={currentIndex === 0} className="absolute left-0 top-1/2 h-24 text-3xl bg-white p-2">
-            {currentIndex === 0 ? <FaTimes /> : <FaAngleLeft />}
+            <button onClick={() => handlePrev("bestSellers")}
+            disabled={bestSellersIndex === 0} className="absolute left-0 top-1/2 h-24 text-3xl bg-white p-2">
+            {bestSellersIndex === 0 ? <FaTimes /> : <FaAngleLeft />}
             </button>
-            <button onClick={handleNext} disabled={currentIndex >= books.length - 6} className="absolute right-0 top-1/2 h-24 text-3xl bg-white p-2">
-            {currentIndex >= books.length - 7 ? <FaTimes /> : <FaAngleRight />}
+            <button  onClick={() => handleNext("bestSellers", books.length)}
+            disabled={bestSellersIndex >= books.length - 7} className="absolute right-0 top-1/2 h-24 text-3xl bg-white p-2">
+            {bestSellersIndex >= books.length - 7 ? <FaTimes /> : <FaAngleRight />}
             </button>
           
         </div>
         {/* cards */}
         <div className="mt-5 grid grid-cols-2 gap-5 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7">
-        {books.slice(currentIndex,currentIndex+7).map((book, idx) => (
+        {books.slice(bestSellersIndex,bestSellersIndex+7).map((book, idx) => (
             <Card key={idx} book={book} />
           ))}
         </div>
@@ -127,17 +162,19 @@ const HomePage = () => {
           <Label name="New published books" />
           {/* pagination / carousel */}
           
-            <button onClick={handlePrev} disabled={currentIndex === 0} className="absolute left-0 top-1/2 h-24 text-3xl bg-white p-2">
-            {currentIndex === 0 ? <FaTimes /> : <FaAngleLeft />}
+            <button onClick={() => handlePrev("newPublished")}
+            disabled={newPublishedIndex === 0} className="absolute left-0 top-1/2 h-24 text-3xl bg-white p-2">
+            {newPublishedIndex === 0 ? <FaTimes /> : <FaAngleLeft />}
             </button>
-            <button onClick={handleNext} disabled={currentIndex >= books.length - 6} className="absolute right-0 top-1/2 h-24 text-3xl bg-white p-2">
-            {currentIndex >= books.length - 7 ? <FaTimes /> : <FaAngleRight />}
+            <button  onClick={() => handleNext("newPublished", books.length)}
+            disabled={newPublishedIndex >= books.length - 7} className="absolute right-0 top-1/2 h-24 text-3xl bg-white p-2">
+            {newPublishedIndex >= books.length - 7 ? <FaTimes /> : <FaAngleRight />}
             </button>
           
         </div>
         {/* cards */}
         <div className="mt-5 grid grid-cols-2 gap-5 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7">
-        {books.slice(currentIndex,currentIndex+7).map((book, idx) => (
+        {books.slice(newPublishedIndex,newPublishedIndex+7).map((book, idx) => (
             <Card key={idx} book={book} />
           ))}
         </div>
