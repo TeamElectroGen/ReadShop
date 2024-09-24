f67e2a6a2a84d289b12cfad6ecae6a20773f1efd
Abdullah Al Maksud <maksud.workspace@gmail.com>
2024-09-24 14:40:02 +0600

    update: footer and contact-us page completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,128 @@
-import React from "react";
+"use client";
+
+import Link from "next/link";
+import React, { useState } from "react";
+import { FaFacebook, FaInstagram, FaLocationArrow, FaXTwitter, FaYoutube } from "react-icons/fa6";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import toast, { Toaster } from "react-hot-toast";
 
 const Footer = () => {
+  const [email, setEmail] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(false);
+
+  // Function to validate the email format
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  // Handle email input change
+  const handleEmailChange = (e) => {
+    const newEmail = e.target.value;
+    setEmail(newEmail);
+    setIsValidEmail(validateEmail(newEmail));
+  };
+
+  // Handle form submission
+  const handleSubscribe = () => {
+    if (isValidEmail) {
+      console.log(`Subscribed email: ${email}`);
+      toast.success("Subscribed successfully!");
+    }
+  };
+
   return (
-    <footer className="mt-10 w-full bg-mediumGray-500/50 pt-3 text-xs">
-      <div className="mx-auto w-11/12">
-        <div className="flex flex-col justify-between gap-3 md:flex-row">
-          <div className="flex min-h-32 w-full items-center justify-center bg-lightGray-200 px-2 py-2 text-center">
-            Logo & Address
-          </div>
-          <div className="flex w-full flex-col gap-3 text-center">
-            <div className="min-h-16 w-full bg-lightGray-200 py-2 md:min-h-20">
-              Newsletter
+    <footer className="w-full bg-darkGray-800 pt-10 rounded-t-sm -mt-2">
+      <div className="mx-auto w-11/12 md:container">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 text-accent">
+          {/* Logo and Address */}
+          <div className="w-full">
+            <div className="flex flex-col">
+              <Link href={'/'} className="text-3xl font-bold font-sans relative w-fit">
+                <h2>
+                  ReadSh<span className="text-red-100">op</span>
+                </h2>
+                <span className="min-w-20 min-h-1 bg-red-600 absolute bottom-0"></span>
+                <span className="min-w-6 min-h-6 border-2 border-red-600 rounded-full  absolute right-[1.15rem] bottom-1 rotate-12"></span>
+                <span className="min-w-6 min-h-6 border-2 border-red-600 rounded-full  absolute -right-[.1rem] bottom-1 rotate-6"></span>
+                <span className="min-w-5 min-h-5 border-b-4 border-red-600 rounded-full  absolute right-[.81rem] -bottom-2 rotate-12"></span>
+              </Link>
+              <div className="pt-2 flex items-center gap-2">
+                <FaLocationArrow />
+                <p>51/A, Kamal Ataturk Avenue, Dhaka-1218</p>
+              </div>
             </div>
-            <div className="min-h-10 w-full bg-lightGray-200 py-2">
-              Social Links
+          </div>
+          {/* Links */}
+          <div className="w-full">
+            <div className="grid grid-cols-2 text-sm">
+              <div className="flex flex-col">
+                <p className="text-lg font-bold pb-3">Pages:</p>
+                <span></span>
+                <Link href={'/'} className="w-fit">Home</Link>
+                <Link href={'/about'} className="w-fit">About</Link>
+                <Link href={'/contact-us'} className="w-fit">Contact us</Link>
+                <Link href={''} className="w-fit">Page 4</Link>
+                <Link href={''} className="w-fit">Page 5</Link>
+                <Link href={''} className="w-fit">Page 6</Link>
+                <Link href={''} className="w-fit">Page 7</Link>
+                <span></span>
+              </div>
+              <div className="flex flex-col">
+                <p className="text-lg font-bold pb-3">Category:</p>
+                <Link href={''} className="w-fit">Page 1</Link>
+                <Link href={''} className="w-fit">Page 2</Link>
+                <Link href={''} className="w-fit">Page 3</Link>
+                <Link href={''} className="w-fit">Page 4</Link>
+                <Link href={''} className="w-fit">Page 5</Link>
+              </div>
+            </div>
+          </div>
+          {/* Newsletter & Social */}
+          <div className="w-full">
+            <div className="border px-5 pb-2 rounded-md w-full mb-4">
+              <h2 className="md:text-sm lg:text-lg font-bold pt-5 text-center">
+                Subscribe to get our <span className="text-red-600">Newsletter</span> weekly!
+              </h2>
+              <p className="text-xs text-center pb-5">We don't send any spam mail, your email will be safe with us!</p>
+              <div className="flex flex-col lg:flex-row border rounded-md py-0.5 px-0.5">
+                <Input
+                  placeholder="Email"
+                  className="border-none"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+                <Button
+                  className="bg-red-600"
+                  onClick={handleSubscribe}
+                  disabled={!isValidEmail}
+                >
+                  Subscribe
+                </Button>
+              </div>
+            </div>
+            <div className="flex gap-4 items-center justify-center lg:justify-start">
+              <p className="">Follow Us:</p>
+              <div className="flex gap-4 text-xl">
+                <Link href={''}><FaFacebook /></Link>
+                <Link href={''}><FaInstagram /></Link>
+                <Link href={''}><FaYoutube /></Link>
+                <Link href={''}><FaXTwitter /></Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
       <div className="mt-3 flex items-center justify-center bg-mediumGray-500 py-2 text-xs text-white">
-        <p>Copyright</p>
+        <p>
+          Copyright © {new Date().getFullYear()} by{" "}
+          <Link href={'/'} className="underline">
+            Readshop Inc.
+          </Link>
+        </p>
       </div>
+      <Toaster />
     </footer>
   );
 };
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,96 +1,211 @@
-import React from "react";
-
+"use client";
+import React, { useEffect, useState } from "react";
+import contactBg from "../../public/assets/contact-us.svg";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { FaAngleDown, FaEnvelope, FaLocationArrow, FaPhone, FaTelegram, FaWhatsapp } from "react-icons/fa6";
+import toast, { Toaster } from "react-hot-toast";
 const ContactUs = () => {
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const watchFields = watch();
+
+  useEffect(() => {
+    const areFieldsValid =
+      watchFields.name &&
+      watchFields.email &&
+      watchFields.phone &&
+      watchFields.issues &&
+      watchFields.message &&
+      !Object.keys(errors).length;
+
+    setIsButtonDisabled(!areFieldsValid);
+  }, [watchFields, errors]);
+
+  const onSubmit = (data) => {
+    console.log(data);
+    toast.success("Message sent successfully!");
+    reset();
+  };
+
   return (
-    <div className="mx-auto max-w-6xl">
-      {/*  */}
-      <div className="bg-lightGray-400 p-4">
-        <div className="mx-auto w-1/4 bg-lightGray-500 text-center">
-          write something
+    <section className="">
+      <Toaster />
+      <div className="flex justify-end relative h-[600px] items-end overflow-clip -mt-16">
+
+        <div className="-z-50 hidden lg:flex relative -left-10 lg:-left-10 -bottom-[380px]">
+          <Image className="" height={1500} src={contactBg} alt="Contact Form Background" />
         </div>
-        <div className="mt-4 h-full max-w-7xl flex-row-reverse gap-4 md:flex lg:flex">
-          <div className="ml-6 w-1/2">
-            <form className="flex flex-col gap-4">
-              <input
-                className="w-full border border-gray-300 p-2"
-                type="text"
-                name="name"
-                required
-              />
-              <input
-                className="border border-gray-300 p-2"
-                type="text"
-                name="email"
-                required
-              />
-              <input
-                className="border border-gray-300 p-2"
-                type="text"
-                name="phone"
-                required
-              />
-              <select
-                className="border border-gray-300 p-2"
-                name="issues"
-                id=""
-                defaultValue={"Select Subject"}
-                required
-              >
-                <option value="Select Subject">Select Subject</option>
-                <option value=" Order Related Issue ">
-                  Order Related Issue
-                </option>
-                <option value=" Payment/Balance/Wallet/Refund ">
-                  Payment/Balance/Wallet/Refund
-                </option>
-                <option value=" Delivery Related Issue ">
-                  Delivery Related Issue
-                </option>
-                <option value=" Product-Related info ">
-                  Product-Related info
-                </option>
-                <option value=" Product Info Missing/Mistake ">
-                  Product Info Missing/Mistake
-                </option>
-              </select>
-              <textarea
-                className="border border-gray-300 p-2"
-                name="message"
-                id=""
-                cols={30}
-                rows={7}
-              ></textarea>
-              <button className="bg-green-600 p-4" style={{ width: "100px" }}>
-                Submit
-              </button>
-            </form>
+
+        <main className="w-[800px] lg:pr-10 bg-[url('../../public/assets/contact-us.svg')] lg:bg-none bg-cover bg-opacity-50">
+          <div className="bg-white/20 backdrop-blur-sm mx-4 mb-2">
+            <div className="p-8 rounded-b-lg shadow-md">
+              <div className="flex justify-center gap-2 mb-4">
+                <div className="w-full">
+                  <h2 className="text-right text-sm">Get in touch</h2>
+                  <h2 className="text-right text-2xl font-semibold mb-4">Send a message</h2>
+                </div>
+              </div>
+              <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+                {/* Name Input */}
+                <div>
+                  <Input
+                    type="text"
+                    placeholder="Your Name"
+                    {...register("name", { required: true })}
+                    className={`border ${errors.name ? "border-red-500" : "border-gray-300"} bg-white`}
+                  />
+                  {errors.name && <p className="text-red-500">Name is required</p>}
+                </div>
+
+                {/* Email Input */}
+                <div>
+                  <Input
+                    type="email"
+                    placeholder="Your Email"
+                    {...register("email", {
+                      required: true,
+                      pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i,
+                    })}
+                    className={`border ${errors.email ? "border-red-500" : "border-gray-300"} bg-white`}
+                  />
+                  {errors.email && <p className="text-red-500">Valid email is required</p>}
+                </div>
+
+                {/* Phone Input */}
+                <div>
+                  <Input
+                    type="tel"
+                    placeholder="Your Phone"
+                    {...register("phone", { required: true, minLength: 11 })}
+                    className={`border ${errors.phone ? "border-red-500" : "border-gray-300"} bg-white`}
+                  />
+                  {errors.phone && <p className="text-red-500">Valid phone number is required</p>}
+                </div>
+
+                {/* Subject Select */}
+                <div>
+                  <select
+                    className={`border p-2 ${errors.issues ? "border-red-500" : "border-gray-300"} rounded-sm w-full`}
+                    {...register("issues", { required: true })}
+                  >
+                    <option value="">Select Subject</option>
+                    <option value="Order Related Issue">Order Related Issue</option>
+                    <option value="Payment/Balance/Wallet/Refund">Payment/Balance/Wallet/Refund</option>
+                    <option value="Delivery Related Issue">Delivery Related Issue</option>
+                    <option value="Product-Related Info">Product-Related Info</option>
+                    <option value="Product Info Missing/Mistake">Product Info Missing/Mistake</option>
+                  </select>
+                  {errors.issues && <p className="text-red-500">Please select a subject</p>}
+                </div>
+
+                {/* Message Textarea */}
+                <div>
+                  <textarea
+                    placeholder="Your Message"
+                    {...register("message", { required: true })}
+                    className={`border ${errors.message ? "border-red-500" : "border-gray-300"} w-full resize-none rounded-sm px-3 py-1 outline-none`}
+                    rows={4}
+                  ></textarea>
+                  {errors.message && <p className="text-red-500">Message is required</p>}
+                </div>
+
+                {/* Submit Button */}
+                <Button type="submit" className="" disabled={isButtonDisabled}>
+                  Submit
+                </Button>
+              </form>
+            </div>
           </div>
-          <div className="w-1/2">
-            {/* image will be added here */}
-            <div className="relative h-24 w-24"></div>
-          </div>
+        </main>
+      </div>
+
+      <div className="relative">
+        <hr className="border-2 border-purple-900 z-0" />
+        <div className="bg-lightGray-400 w-fit p-2 rounded-full absolute z-10 -bottom-3.5 left-1/2">
+          <FaAngleDown />
         </div>
       </div>
 
-      {/* map section */}
+      <div className="bg-purple-100">
+        <div className="w-11/12 md:container mx-auto flex flex-col lg:flex-row py-10 rounded-sm gap-5">
 
-      <section>
-        <div className="mt-5 h-[70vh] border bg-lightGray-400">
-          <div className="mx-auto mt-4 w-1/4 bg-lightGray-500 p-3 text-center">
-            <h1>Text will appear</h1>
+          <div className="w-full flex flex-col gap-5">
+
+            <div className="bg-violet-200/50 rounded-sm p-5">
+              <div className="flex gap-3 mb-5">
+                <div className="w-1  bg-purple-800">
+                </div>
+                <div>
+                  <h2 className="font-semibold text-2xl">Connect</h2>
+                  <p className="text-purple-950">Reach us with your question!</p>
+                </div>
+              </div>
+              <div className="grid md:grid-cols-2 gap-3">
+                <div className="flex items-center gap-4 border rounded-sm">
+                  <div className="bg-white/50 w-fit p-2 rounded-md">
+                    <FaEnvelope />
+                  </div>
+                  <p>info@readshop.com</p>
+                </div>
+                <div className="flex items-center gap-4 border rounded-sm">
+                  <div className="bg-white/50 w-fit p-2 rounded-md">
+                    <FaPhone />
+                  </div>
+                  <p>(+880) 1000 00 00 00</p>
+                </div>
+                <div className="flex items-center gap-4 border rounded-sm">
+                  <div className="bg-white/50 w-fit p-2 rounded-md">
+                    <FaTelegram />
+                  </div>
+                  <p>@readshop.official</p>
+                </div>
+                <div className="flex items-center gap-4 border rounded-sm">
+                  <div className="bg-white/50 w-fit p-2 rounded-md">
+                    <FaWhatsapp />
+                  </div>
+                  <p>(+880) 1000 00 00 00</p>
+                </div>
+              </div>
+            </div>
+            <div className="bg-yellow-200/50 rounded-sm p-5">
+              <div className="flex gap-3 mb-5">
+                <div className="w-1  bg-yellow-800">
+
+                </div>
+                <div>
+
+                  <h2 className="font-semibold text-2xl">Address</h2>
+                  <p className="text-purple-950">Where we live!</p>
+                </div>
+              </div>
+              <div className="gap-3">
+                <div className="flex items-center gap-4 border rounded-sm">
+                  <div className="bg-white/50 w-fit p-2 rounded-md">
+                    <FaLocationArrow />
+                  </div>
+                  <p>51/A, Kamal Ataturk Avenue, Dhaka-1218</p>
+                </div>
+              </div>
+            </div>
+
           </div>
-
-          <div className="mx-auto mt-4 flex w-full items-center gap-4 p-2">
-            <div className="h-72 w-1/2 bg-lightGray-500">
-              {/* Content for this div */}
-            </div>
-            <div className="flex h-72 w-1/2 items-center justify-center bg-lightGray-100">
-              <h1>Map</h1>
-            </div>
+          <div className="w-full">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d51905.734490399715!2d90.40939901317851!3d23.801212593519956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1727093620013!5m2!1sen!2sbd" allowfullscreen="" loading="lazy" className="rounded-sm border shadow w-full h-80 lg:h-full" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
