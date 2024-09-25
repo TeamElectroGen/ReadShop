7be3c57ada69ae022f76d15eceb51d2da3678dd5
masud4657 <masudpfrnt@gmail.com>
2024-09-25 16:06:20 +0600

    Fix error in details page.

--- src/pages/ViewDetails.jsx
+++ src/pages/ViewDetails.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const ViewDetails = () => {
-    return <div className="text-3xl text-blue-500">Testing Tailwind</div>;
+    return (
+        <div className="bg-blue-500 text-white p-4">
+          <p className="text-3xl">view details</p>
+        </div>
+      );
 }
 
 export default ViewDetails;
