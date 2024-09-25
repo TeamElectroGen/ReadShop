c8ff0a337e324eb93c7019afc765fe57c6393356
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-09-25 22:37:16 +0600

    add: getBooksData.js file created

---
+++ src/services/getBooksData.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const getAllBooks = async () => {
+  try {
+    const res = await axios(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/all-books`
+    );
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+export const getBookDetails = async (id) => {
+  try {
+    const res = await axios(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/all-books/${id}`
+    );
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return {};
+  }
+};
