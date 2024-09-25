13186702b8dbe75382821c38446d759c74cb933d
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-09-25 21:15:12 +0600

    Update: tailwind container configeration

--- tailwind.config.js
+++ tailwind.config.js
@@ -83,9 +83,6 @@ module.exports = {
       center: true,
       padding: {
         DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
         '2xl': '10rem',
       },
     },
