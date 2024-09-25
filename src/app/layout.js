5ca305e46f6afd243ce12927cc36751669dba1e9
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-09-25 20:00:54 +0600

    update: authProvider file's location changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
-import AuthProvider from "@/AuthProvider";
+import AuthProvider from "@/services/AuthProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
