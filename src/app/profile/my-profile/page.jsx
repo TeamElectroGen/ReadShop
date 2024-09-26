200e494b45cabc2b4f104a47bbd8319e4dc4153e
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-09-26 19:07:50 +0600

    Add: my profile page in profile route

---
+++ src/app/profile/my-profile/page.jsx
@@ -0,0 +1,20 @@
+import { Separator } from "@/components/ui/separator";
+import React from "react";
+import ProfileForm from "./profile-form";
+
+const MyProfilePage = () => {
+  return (
+    <div className="space-y-6 bg-background shadow-sm rounded-lg p-6">
+      <div>
+        <h3 className="text-lg font-medium">Profile</h3>
+        <p className="text-sm text-muted-foreground">
+          This is how others will see you on the site.
+        </p>
+      </div>
+      <Separator />
+      <ProfileForm />
+    </div>
+  );
+};
+
+export default MyProfilePage;
