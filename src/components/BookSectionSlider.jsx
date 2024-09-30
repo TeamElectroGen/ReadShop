d13fc09200dc56cf3d7f8da18e6b99f2858d870e
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-09-30 12:39:22 +0600

    Update: book section slider ui

--- src/components/BookSectionSlider.jsx
+++ src/components/BookSectionSlider.jsx
@@ -5,69 +5,67 @@ import "swiper/css";
 import "swiper/css/navigation";
 import Link from "next/link";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
+import { Button } from "./ui/button";
 
 const BookSectionSlider = ({ items, renderCard, viewAllLink }) => {
-    const swiperRef = useRef(null);
+  const swiperRef = useRef(null);
 
-    const handlePrev = () => {
-        if (swiperRef.current && swiperRef.current.swiper) {
-            swiperRef.current.swiper.slidePrev();
-        }
-    };
+  const handlePrev = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slidePrev();
+    }
+  };
 
-    const handleNext = () => {
-        if (swiperRef.current && swiperRef.current.swiper) {
-            swiperRef.current.swiper.slideNext();
-        }
-    };
+  const handleNext = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slideNext();
+    }
+  };
 
-    return (
-        <div className="relative slider-container">
-            {/* Navigation Buttons */}
-            <div className="absolute -mt-14 md:-top-2 gap-2 -right-2 md:right-0 z-10 flex p-2">
-                <button
-                    onClick={handlePrev}
-                    className="bg-primary hover:shadow-md text-black font-bold text-sm px-2 py-1 md:py-2 md:px-4 rounded"
-                >
-                    <FaArrowLeft />
-                </button>
-                <button
-                    onClick={handleNext}
-                    className="bg-primary bg-pri hover:shadow-md text-black font-bold text-sm px-2 py-1 md:py-2 md:px-4 rounded"
-                >
-                    <FaArrowRight />
-                </button>
-            </div>
+  return (
+    <div className="slider-container relative">
+      {/* Navigation Buttons */}
+      <div className="absolute -right-2 z-10 -mt-14 flex gap-2 md:-top-3 md:right-0">
+        <Button onClick={handlePrev} variant="outline" size="icon" className="hover:bg-primary/60">
+          <FaArrowLeft />
+        </Button>
+        <Button onClick={handleNext} variant="outline" size="icon" className="hover:bg-primary/60">
+          <FaArrowRight />
+        </Button>
+      </div>
 
-            {/* Swiper Slider */}
-            <Swiper
-                ref={swiperRef}
-                spaceBetween={20}
-                slidesPerView={2}
-                navigation={false}
-                modules={[Navigation]}
-                breakpoints={{
-                    640: { slidesPerView: 2, spaceBetween: 10 },
-                    768: { slidesPerView: 3, spaceBetween: 20 },
-                    1024: { slidesPerView: 5, spaceBetween: 30 },
-                    1280: { slidesPerView: 5, spaceBetween: 20 },
-                }}
+      {/* Swiper Slider */}
+      <Swiper
+        ref={swiperRef}
+        spaceBetween={20}
+        slidesPerView={2}
+        navigation={false}
+        modules={[Navigation]}
+        breakpoints={{
+          640: { slidesPerView: 2, spaceBetween: 10 },
+          768: { slidesPerView: 3, spaceBetween: 20 },
+          1024: { slidesPerView: 4, spaceBetween: 30 },
+          1280: { slidesPerView: 5, spaceBetween: 20 },
+        }}
+      >
+        {/* Render all the book items */}
+        {items?.map((item, idx) => (
+          <SwiperSlide key={idx}>{renderCard(item)}</SwiperSlide>
+        ))}
+        {/* Add a "View All" button as the last card */}
+        <SwiperSlide className="rounded-md bg-secondary">
+          <div className="flex h-96 items-center justify-center">
+            <Link
+              href={`${viewAllLink}`}
+              className="rounded-md bg-primary-foreground/80 px-5 py-2 text-white hover:bg-primary-foreground"
             >
-                {/* Render all the book items */}
-                {items.map((item, idx) => (
-                    <SwiperSlide key={idx}>{renderCard(item)}</SwiperSlide>
-                ))}
-                {/* Add a "View All" button as the last card */}
-                <SwiperSlide className="bg-secondary rounded-md">
-                    <div className="flex items-center justify-center h-96">
-                        <Link href={`${viewAllLink}`} className="bg-primary-foreground/80 px-5 py-2 rounded-md text-white hover:bg-primary-foreground">
-                            View All
-                        </Link>
-                    </div>
-                </SwiperSlide>
-            </Swiper>
-        </div>
-    );
+              View All
+            </Link>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
 
 export default BookSectionSlider;
