a8c6cc63c2d08f96fb048af7737d7cf80cb0023b
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-10-01 15:51:06 +0600

    New Tech (Nextjs) add: getBooksByIds function created. update: comment added for each function that how they work

--- src/services/getBooksData.js
+++ src/services/getBooksData.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// Fetches all books from the API
 export const getAllBooks = async () => {
   try {
     const res = await axios(
@@ -12,6 +13,7 @@ export const getAllBooks = async () => {
   }
 };
 
+// Retrieves details of a specific book by its ID
 export const getBookDetails = async (id) => {
   try {
     const res = await axios(
@@ -24,6 +26,7 @@ export const getBookDetails = async (id) => {
   }
 };
 
+// Gets the read/wish status of a book for a specific user
 export const getReadWishStatusUser = async (bookId, email) => {
   try {
     const res = await axios(
@@ -37,15 +40,29 @@ export const getReadWishStatusUser = async (bookId, email) => {
   }
 };
 
+// Updates the read/wish list status for a book and user
 export const patchRWList = async (which, bookId, email) => {
   try {
     const res = await axios.patch(
       `${process.env.NEXT_PUBLIC_BASE_URL}/api/private/wish-read/${which}-toggle-update?bookId=${bookId}&email=${email}`
     );
-    console.log(res);
     return res;
   } catch (error) {
     console.log(error);
     return error;
+  }
+};
+
+// Fetches all books by an array of IDs
+export const getBooksByIds = async (ids) => {
+  try {
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/books-by-ids`,
+      { ids }
+    );
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return [];
   }
 };
