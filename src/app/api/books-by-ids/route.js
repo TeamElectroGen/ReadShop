423387b037f937548b29c7e6d0a0757fe972f559
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-10-01 15:11:52 +0600

    New Tech (Nextjs) add: get api created for specific array of books id.

---
+++ src/app/api/books-by-ids/route.js
@@ -0,0 +1,23 @@
+import { connectDB } from "@/lib/connectDB";
+import { ObjectId } from "mongodb";
+import { NextResponse } from "next/server";
+
+export const GET = async (request) => {
+  const { ids } = await request.json();
+
+  const db = await connectDB();
+  const booksCollection = await db.collection("books");
+  try {
+    if (!ids) {
+      return NextResponse.json({ error: "Ids are required!" });
+    }
+    const books = await booksCollection
+      .find({ _id: { $in: ids.map((id) => new ObjectId(id)) } })
+      .toArray();
+    console.log(books);
+    return NextResponse.json({ books });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: "Something went wrong!" });
+  }
+};
