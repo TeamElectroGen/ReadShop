4d4d59e287f45e6980519ec591271e15eab7e5af
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-10-02 12:49:53 +0600

    Email-verification page added

--- src/components/EmailVerify.jsx
+++ src/components/EmailVerify.jsx
@@ -1,36 +1,42 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 const EmailVerify = () => {
-    return (
-        <div>
-             <div className="flex items-center justify-center h-screen bg-purple-100">
-      <div className="bg-white p-10 rounded-lg shadow-lg max-w-sm w-full text-center">
-        <div className="bg-purple-200 rounded-full w-20 h-20 flex items-center justify-center mx-auto mb-4">
-        <Image
-            src="/path/to/envelope-icon.png" //TODO: Icon will be added here
-            alt="Envelope Icon"
-            width={52} 
-            height={52} 
-            className="w-12 h-12"
-          />
+  return (
+    <div>
+      <div className="flex min-h-screen items-center justify-center to-white">
+        <div className="w-full max-w-sm rounded-lg bg-white p-8 text-center shadow-lg">
+          <div className="mx-auto mb-4 flex h-20 w-20 items-center justify-center rounded-full bg-sky-50">
+            <Image
+              src="/assets/messageBox.png"
+              alt="Message Icon"
+              width={62}
+              height={62}
+              className="h-12 w-12"
+            />
+          </div>
+          <h2 className="mb-2 text-xl font-semibold text-gray-800">
+            Check your inbox, please!
+          </h2>
+          <p className="mb-4 text-gray-600">
+            
+            Hey Nazrul, to start using this, we need to verify your email. We’ve
+            already sent out the verification link. Please check it and confirm
+            it's really you.
+          </p>
+          <button className="rounded-lg bg-blue-500 px-4 py-2 text-white transition duration-200 hover:bg-blue-400">
+            Sure!
+          </button>
+          <p className="mt-4 text-gray-500">
+            Didn’t get e-mail?{" "}
+            <a href="#" className="text-blue-500 hover:underline">
+              Send it again
+            </a>
+          </p>
         </div>
-        <h2 className="text-xl font-semibold text-gray-800 mb-2">Check your inbox, please!</h2>
-        <p className="text-gray-600 mb-4">
-          Hey Nazrul, to start using this, we need to verify your email.
-          We’ve already sent out the verification link. Please check it and
-          confirm it's really you.
-        </p>
-        <button className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-400 transition duration-200">
-          Sure!
-        </button>
-        <p className="text-gray-500 mt-4">
-          Didn’t get e-mail? <a href="#" className="text-blue-500 hover:underline">Send it again</a>
-        </p>
       </div>
     </div>
-        </div>
-    );
+  );
 };
 
 export default EmailVerify;
