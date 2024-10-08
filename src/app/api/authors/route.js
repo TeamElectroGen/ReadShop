a3c262c59b41b2e2474432562e53e3c47f612c9c
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-10-08 11:27:50 +0600

    new Tech (Nextjs) add: get api created for getting all authors

---
+++ src/app/api/authors/route.js
@@ -0,0 +1,14 @@
+import { connectDB } from "@/lib/connectDB";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  const db = await connectDB();
+  const authorsCollection = await db.collection("authors");
+  try {
+    const authors = await authorsCollection.find().toArray();
+    return NextResponse.json({ authors });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "No Data Found!", error });
+  }
+};
