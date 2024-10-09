75e3f2beba843d92380cc28892385e1eb9852696
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-10-09 12:05:13 +0600

    add: create function for getting user review and rating data

--- src/services/reviewAndRating.js
+++ src/services/reviewAndRating.js
@@ -13,3 +13,16 @@ export const postReviewAndRating = async (email, bookId, reviewAndRating) => {
     return {};
   }
 };
+
+// get review and rating api for user
+export const getUserReviewAndRating = async (email, bookId) => {
+  try {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/private/get-user-rr/${email}?bookId=${bookId}`
+    );
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return {};
+  }
+};
