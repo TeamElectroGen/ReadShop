86963341fda68e93387cf9ebe9b44ad51ed4ca68
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-10-09 10:00:27 +0600

    Add: PublisherMenu component in nav

--- src/components/DashboardSidebar.jsx
+++ src/components/DashboardSidebar.jsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import LogoutButton from "@/components/LogoutButton";
 import SiteLogo from "@/components/SiteLogo";
 import AdminMenu from "./AdminMenu";
+import PublisherMenu from "./PublisherMenu";
 
 const DashboardSidebar = () => {
   const role = "admin";
@@ -15,7 +16,7 @@ const DashboardSidebar = () => {
     <aside className="hidden shadow-lg bg-background md:block sticky top-0 bottom-0 left-0 z-50">
       <div className="flex h-full max-h-screen flex-col gap-6">
         {/* logo */}
-        <header className="flex h-14 pt-6 items-center px-4 lg:h-[60px] lg:px-6">
+        <header className="flex h-14 pt-3 items-center px-4 lg:h-[60px] lg:px-6">
           <SiteLogo />
           <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
             <Bell className="h-4 w-4" />
@@ -26,6 +27,7 @@ const DashboardSidebar = () => {
         <div className="flex-1">
           <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
             {role === "admin" && <AdminMenu />}
+            {role === "publisher" && <PublisherMenu />}
           </nav>
         </div>
         {/* end */}
