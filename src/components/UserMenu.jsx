536f78bd6c50b9793511174cae6951ccf4cb3998
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-10-10 19:21:27 +0600

    Remove: extra logout button

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -38,12 +38,6 @@ const UserMenu = () => {
           icon={item.icon}
         />
       ))}
-      <button
-        onClick={() => signOut()}
-        className="flex cursor-pointer items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-foreground"
-      >
-        <LogOut className="size-4" /> Logout
-      </button>
     </>
   );
 };
