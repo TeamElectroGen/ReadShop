39cd2a2071d3f6c048da9aa16fe61dffaf55d2a8
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-10-13 00:06:29 +0600

    add: loading component added for data fetching

---
+++ src/components/BookLoading.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const BookLoading = () => {
+  return (
+    <div class="loader md:!size-12">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default BookLoading;
