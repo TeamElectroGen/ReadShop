f86adb53f6993a4959d1613fa10368e1773b35ae
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-10-12 18:09:33 +0600

    New Tech (Nextjs) update: dynamic user data injected all reviewAndRating Data for specific book.

--- src/app/api/(reviewAndRating)/get-all-rr-of-book/[bookId]/route.js
+++ src/app/api/(reviewAndRating)/get-all-rr-of-book/[bookId]/route.js
@@ -1,4 +1,5 @@
 import { connectDB } from "@/lib/connectDB";
+import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
@@ -7,14 +8,32 @@ export const GET = async (request, { params }) => {
   const db = await connectDB();
   const { bookId } = params;
   const reviewAndRatingCollection = db.collection("reviewAndRating");
+  const usersCollection = db.collection("users");
   if (!bookId) {
     return NextResponse.json({ message: "forbidden access!" });
   }
-    // TODO: get single user data and insert it in the reviewAndRatingData for dynamic user info.
+
   try {
-    const reviewAndRatingData = await reviewAndRatingCollection
+    let reviewAndRatingData = await reviewAndRatingCollection
       .find({ bookId })
       .toArray();
+
+    // Add user information to each review
+    reviewAndRatingData = await Promise.all(
+      reviewAndRatingData.map(async (review) => {
+        const user = await usersCollection.findOne({
+          _id: new ObjectId(review.userId),
+        });
+        return {
+          ...review,
+          user: {
+            name: user.name,
+            email: user.email,
+            image: user.image,
+          },
+        };
+      })
+    );
     return NextResponse.json({ reviewAndRatingData });
   } catch (error) {
     return NextResponse.json({ message: "Failed to get" });
