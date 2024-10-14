c4c51904850d8c092bfcb8ddda57e4480129cd89
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-10-14 16:23:03 +0600

    New Feature(payment system) add: backend setup for book purchasing checkout on checkout page

--- src/app/checkout/page.jsx
+++ src/app/checkout/page.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+"use client";
+import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "@/components/CheckoutForm";
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
 
 const Page = () => {
+  const totalPrice = 90;
   return (
     <div>
-      Checkout
+      <Elements stripe={stripePromise}>
+        <CheckoutForm totalPrice={totalPrice}></CheckoutForm>
+      </Elements>
     </div>
   );
-}
+};
 
 export default Page;
