e4db0e3b07bf8d870b047a101e8f4fe32eb2a623
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-10-16 11:07:03 +0600

    Update: EmptyCard to get dynamic props

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -4,7 +4,7 @@ import emptyCartAnimation from "../../public/emptyCart";
 import { Button } from "./ui/button";
 import Link from "next/link";
 
-const EmptyCart = () => {
+const EmptyCart = ({title="", desc=""}) => {
   return (
     <div className="flex min-h-96 items-center justify-center rounded-lg border border-dashed bg-background shadow-sm">
       <div className="flex flex-col items-center gap-1 text-center">
@@ -14,10 +14,10 @@ const EmptyCart = () => {
           loop={true}
         />
         <h3 className="-mt-7 text-xl font-bold tracking-tight">
-          Your cart is currently empty
+          {title}
         </h3>
         <p className="text-sm text-muted-foreground">
-          You can start selling as soon as you add a product.
+          {desc}
         </p>
         <Button asChild className="mt-4 font-semibold">
           <Link href={"/"}>Browser books</Link>
