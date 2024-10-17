1382644ce865a8a8bd8c2a4dfbbea0b225785ceb
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-10-17 18:46:33 +0600

    fixed: type error fixed

--- src/services/payment.js
+++ src/services/payment.js
@@ -3,8 +3,8 @@ import axios from "axios";
 // post payment data to the database
 export const postPaymentData = async (paymentInfo) => {
   try {
-    const res = axios.post(
-      `${process.env.NEXT_PUBLIC_SERVER_URL}/checkout/api/post-payment`,
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/checkout/api/post-payment`,
       paymentInfo
     );
     return res.data;
