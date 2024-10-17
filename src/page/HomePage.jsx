cd75c2c23d3a6d26f1d10089cc6943840613ec41
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-10-17 18:31:11 +0600

    Update: author section's card

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -229,6 +229,8 @@ const HomePage = () => {
           renderCard={(book) => <Card book={book} />} // Pass how you want to render the card
         />
       </section>
+
+      {/* Author section */}
       <section className="z-10 mt-10 rounded-xl border-b-4 border-primary bg-white/20 p-8 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.2)] backdrop-blur">
         <AuthorSectionTitle title={"Author"} />
         <AuthorSectionSlide items={authors?.slice(0, 10)} />
