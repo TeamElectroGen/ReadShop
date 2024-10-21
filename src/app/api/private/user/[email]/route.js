ed668e2d805b4768c337367a40e96a150c28bcd5
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-10-21 18:15:52 +0600

    Update: projection of GET api for user data

--- src/app/api/private/user/[email]/route.js
+++ src/app/api/private/user/[email]/route.js
@@ -7,7 +7,7 @@ export const GET = async (request, { params }) => {
   try {
     const user = await db
       .collection("users")
-      .findOne({ email }, { projection: { _id: 1, name: 1, image: 1 } });
+      .findOne({ email }, { projection: { _id: 1, name: 1, image: 1, email:1, phone:1, address:1, dob:1 } });
     return NextResponse.json({ user });
   } catch (error) {
     console.log(error);
