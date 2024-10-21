694fec184fab8540ee8d0e9ee6439fd77f93e951
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-10-21 18:14:27 +0600

    Fix: not defined error

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -184,7 +184,7 @@ const HomePage = () => {
       {/* New Arrival Book Slider  (Albab updated this section) */}
       <section className="z-10 mt-10 rounded-xl border-b-4 border-primary bg-white/20 p-8 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.2)] backdrop-blur">
         <BookSectionTitle title={"New Arrival"} />
-        {isNewBookFetching ? (
+        {newBookLoading ? (
           <div className="my-10 flex justify-center">
             <div className="h-10 w-10 animate-spin rounded-full border-4 border-t-primary"></div>
           </div>
