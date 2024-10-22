25e7e86bb8e5219cb7b37387e5109eba056bf546
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-10-22 09:20:29 +0600

    New Feature(Chat Bot) update: error handled and some design changed

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -3,26 +3,30 @@ import { MessagesContext } from "@/context/messages";
 import { cn } from "@/lib/utils";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
+import { CornerDownLeft, Loader2 } from "lucide-react";
 import React, { useContext, useRef } from "react";
 import { useState } from "react";
+import toast from "react-hot-toast";
 import TextareaAutosize from "react-textarea-autosize";
 import { v4 as uuidv4 } from "uuid";
 
 const ChatInput = ({ className, ...props }) => {
   const [input, setInput] = useState("");
   const {
-    messages,
+    // messages,
     addMessage,
-    // removeMessage,
-    updateMessage,
+    removeMessage,
+    // updateMessage,
     setIsMessageUpdating,
   } = useContext(MessagesContext);
   const textareaRef = useRef(null);
 
-  const { mutate: sendMessage } = useMutation({
+  const { mutate: sendMessage, isPending } = useMutation({
     mutationFn: async (message) => {
       const res = await axios.post("/api/message", { messages: [message] });
-      console.log(res.data);
+      if (res.status !== 200) {
+        throw new Error("Failed to send message");
+      }
       return res.data;
     },
     onMutate(message) {
@@ -55,27 +59,31 @@ const ChatInput = ({ className, ...props }) => {
 
       setIsMessageUpdating(true);
 
-      const reader = response.getReader();
-      const decoder = new TextDecoder();
-      let done = false;
+      setInput("");
+      // const reader = response.getReader();
+      // const decoder = new TextDecoder();
+      // let done = false;
 
-      while (!done) {
-        const { value, done: doneReading } = await reader.read();
-        done = doneReading;
-        const chunkValue = decoder.decode(value);
-        updateMessage(id, (prev) => prev + chunkValue);
-      }
-
+      // while (!done) {
+      //   const { value, done: doneReading } = await reader.read();
+      //   done = doneReading;
+      //   const chunkValue = decoder.decode(value);
+      //   updateMessage(id, (prev) => prev + chunkValue);
+      // }
       // clean up
       setIsMessageUpdating(false);
-      setInput("");
 
       setTimeout(() => {
         textareaRef.current?.focus();
       }, 10);
     },
+    onError: (_, message) => {
+      toast.error("Something went wrong. Please try again.");
+      removeMessage(message.id);
+      textareaRef.current?.focus();
+    },
   });
-  console.log(messages);
+
   return (
     <div {...props} className={(cn("border-t border-zinc-300"), className)}>
       <div className="relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-none">
@@ -90,17 +98,26 @@ const ChatInput = ({ className, ...props }) => {
                 isUserMessage: true,
                 text: input,
               };
-              setInput("");
               sendMessage(message);
             }
           }}
           maxRows={4}
+          disabled={isPending}
           value={input}
           onChange={(e) => setInput(e.target.value)}
           autoFocus
           placeholder="Write a message..."
           className="peer block w-full resize-none border-0 bg-zinc-100 py-1.5 pl-4 pr-14 text-sm text-gray-900 focus:ring-0 disabled:opacity-50 sm:leading-6"
         />
+        <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
+          <kbd className="inline-flex items-center rounded border border-gray-200 bg-white px-1 font-sans text-xs text-gray-400">
+            {isPending ? (
+              <Loader2 className="h-3 w-3 animate-spin" />
+            ) : (
+              <CornerDownLeft className="h-3 w-3" />
+            )}
+          </kbd>
+        </div>
       </div>
     </div>
   );
