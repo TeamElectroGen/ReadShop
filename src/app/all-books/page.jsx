8125a70811f4d4193407c87db5772e5232c765dc
imranhasan2 <imranhasan030303@gmail.com>
2024-10-23 09:54:36 +0600

    add:metadat for contactus and allbooks

--- src/app/all-books/page.jsx
+++ src/app/all-books/page.jsx
@@ -1,5 +1,12 @@
 import AllBooks from "@/page/AllBooks";
 import React from "react";
+export const generateMetadata = () => {
+  return {
+    title: "All Books | readShop - Explore Our Collection",
+    description:
+      "Browse through the extensive collection of books available at readShop. Find books across various genres, authors, and categories to satisfy your reading cravings.",
+  };
+};
 
 const page = () => {
   return <AllBooks />;
--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -1,5 +1,13 @@
 import ContactUs from "@/page/ContactUs";
 
+export const generateMetadata = () => {
+  return {
+    title: "Contact Us | readShop - We're Here to Help",
+    description:
+      "Get in touch with readShop for any queries, support, or feedback. We're here to assist you with your book discovery and shopping experience. Reach out to us today!",
+  };
+};
+
 const Page = () => {
   return (
     <>
