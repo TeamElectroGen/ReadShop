15e600253dce4dfc94aa4e206c93ebffc669feaa
imranhasan2 <imranhasan030303@gmail.com>
2024-10-23 13:05:37 +0600

    add:dynamic title for the author details page

--- src/app/author/[authorId]/page.jsx
+++ src/app/author/[authorId]/page.jsx
@@ -1,4 +1,11 @@
 import AuthorDetails from "@/page/AuthorDetails";
+export const generateMetadata = () => {
+  return {
+    title: "Authors | readShop - Meet Your Favorite Writers",
+    description:
+      "Discover the authors behind your favorite books at readShop. Explore author profiles, their works, and their journey in the literary world.",
+  };
+};
 
 const Page = ({ params }) => {
   return <AuthorDetails authorId={params.authorId}></AuthorDetails>;
