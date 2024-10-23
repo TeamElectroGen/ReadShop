a96d9cb409c93bfdf42e94d4c8c8cf45d11a7888
imranhasan2 <imranhasan030303@gmail.com>
2024-10-23 09:48:47 +0600

    add:metadata for the about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,14 @@
 import About from "@/page/About";
 import React from "react";
 
+export const generateMetadata = () => {
+  return {
+    title: "About Us | readShop - Your Trusted Bookstore",
+    description:
+      "Learn more about readShop, your trusted destination for discovering, reading, and purchasing your favorite books. Explore our mission, vision, and values.",
+  };
+};
+
 const Page = () => {
   return (
     <>
