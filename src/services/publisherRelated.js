fdeda6a8c5eba1a8bc4dd5580f638a14daff8dbf
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-10-29 12:54:35 +0600

    add: get all books function created for publisher

--- src/services/publisherRelated.js
+++ src/services/publisherRelated.js
@@ -13,3 +13,16 @@ export const postBookData = async (email, bookData) => {
     throw error;
   }
 };
+
+// get specific publisher books data from database
+export const getPublisherBooks = async (email) => {
+  try {
+    const res = await axios(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/publisher/get-publisher-books/${email}`
+    );
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
