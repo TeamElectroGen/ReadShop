db8f36ead8c9a8f9b72549db180a1a90fa26f4aa
Abdullah Al Maksud <maksud.workspace@gmail.com>
2024-10-30 23:56:23 +0600

    fix: category section ui fixed

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -1,15 +1,13 @@
-import { getAllBooks, getCategories } from "@/services/getBooksData"; // Service to get books and categories
-import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useRef, useState } from "react";
 import { Button } from "./ui/button";
-import { Swiper, SwiperSlide } from "swiper/react"; // Import Swiper components
-import "swiper/swiper-bundle.css"; // Import Swiper styles
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
 import { Navigation } from "swiper/modules";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 
-const CategorySection = ({ categoriesName, books: allBooks }) => {
+const CategorySection = ({ books: allBooks }) => {
   const [selectedCategory, setSelectedCategory] = useState("all");
   const swiperRef = useRef(null);
 
@@ -96,37 +94,42 @@ const CategorySection = ({ categoriesName, books: allBooks }) => {
         >
           {filteredBooks?.map((book) => (
             <SwiperSlide key={book._id}>
-              <Link
-                href={`/view-details/${book._id}`}
-                className="relative h-52"
-              >
-                <Image
-                  src={book.CoverImage}
-                  alt={book.BookName}
-                  width={150}
-                  height={220}
-                  className="h-52 w-full rounded-sm object-cover"
-                />
-                <div className="absolute bottom-0 z-10 flex h-14 w-full flex-col rounded-b-sm bg-primary pb-3 text-center shadow-sm">
-                  <h2 className="flex-1 truncate px-2 pt-2 text-sm font-bold">
-                    {book.BookName}
-                  </h2>
-                  <p className="text-xs">{book.AuthorName}</p>
-                </div>
-              </Link>
+              <div>
+                <Link
+                  href={`/view-details/${book?._id}`}
+                  className="relative h-60 hover:cursor-pointer"
+                >
+                  <Image
+                    src={book.CoverImage}
+                    alt={book.BookName}
+                    width={150}
+                    height={220}
+                    className="h-60 w-full rounded-sm object-cover"
+                  />
+                  <div className="absolute bottom-0 z-10 flex h-14 w-full flex-col rounded-b-sm bg-primary pb-3 text-center shadow-sm">
+                    <h2 className="flex-1 truncate px-2 pt-2 text-sm font-bold">
+                      {book.BookName}
+                    </h2>
+                    <p className="text-xs">{book.AuthorName}</p>
+                  </div>
+                </Link>
+              </div>
             </SwiperSlide>
           ))}
         </Swiper>
-        <div className="absolute top-0 z-10 flex h-full w-full items-center justify-between bg-primary/0">
+
+        <div className="absolute top-[42%] z-10 flex h-fit w-fit items-center justify-between">
           <Button
             onClick={handlePrev}
-            className="h-10 w-10 rounded-l-none bg-secondary p-0 shadow-lg shadow-black/30"
+            className="h-10 w-10 rounded-l-none bg-secondary p-0 shadow-lg"
           >
             <FaAngleLeft />
           </Button>
+        </div>
+        <div className="absolute right-0 top-[42%] z-10 flex h-fit w-fit items-center justify-between">
           <Button
             onClick={handleNext}
-            className="h-10 w-10 rounded-r-none bg-secondary p-0 shadow-lg shadow-black/30"
+            className="h-10 w-10 rounded-r-none bg-secondary p-0 shadow-lg"
           >
             <FaAngleRight />
           </Button>
