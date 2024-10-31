82686ff6d7808a6a30e5aecf4e712bbd46ca08dd
imranhasan2 <imranhasan030303@gmail.com>
2024-10-31 13:21:58 +0600

    updated: metaData

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,9 +1,9 @@
 import SignUp from "@/page/SignUp";
 export const generateMetadata = () => {
   return {
-    title: "Sign Up | readShop - Join Our Book Community",
+    title: "Sign Up -Join Our Book Community",
     description:
-      "Create a readShop account to explore a world of books, manage your wishlist, and stay updated with new arrivals and offers. Join our community today!",
+      "Create a ReadShop account to explore a world of books, manage your wishlist, and stay updated with new arrivals and offers. Join our community today!",
   };
 };
 
