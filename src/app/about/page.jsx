fd35aa81a65ecba7858e2cefae86b984399e3176
imranhasan2 <imranhasan030303@gmail.com>
2024-10-31 16:41:42 +0600

    updated: metadata

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,6 +5,13 @@ export const generateMetadata = () => {
     title: "About Us",
     description:
       "Learn more about ReadShop, your trusted destination for discovering, reading, and purchasing your favorite books. Explore our mission, vision, and values.",
+    keywords: [
+      "about us",
+      "about ReadShop",
+      "ReadShop mission",
+      "ReadShop vision",
+      "ReadShop values",
+    ],
   };
 };
 
