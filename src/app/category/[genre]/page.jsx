076587d2bd32850c85b2c02547425db1254d65b9
imranhasan2 <imranhasan030303@gmail.com>
2024-10-31 13:20:32 +0600

    updated: metaData

--- src/app/category/[genre]/page.jsx
+++ src/app/category/[genre]/page.jsx
@@ -2,9 +2,9 @@ import BookOfCategory from "@/page/BookOfCategory";
 export const generateMetadata = ({ params }) => {
   return {
     title: `${params.genre.split("%20").join(" ")} Books`,
-    description: `Discover the best books in the ${params.genre.split("%20").join(" ")} category at readShop. Browse a curated selection of top titles, authors, and reviews to find your next great read.`,
+    description: `Discover the best books in the ${params.genre.split("%20").join(" ")} category at ReadShop. Browse a curated selection of top titles, authors, and reviews to find your next great read.`,
     keywords: [
-      `Discover the best books in the ${params.genre.split("%20").join(" ")} category at readShop. Browse a curated selection of top titles, authors, and reviews to find your next great read.`.split(
+      `Discover the best books in the ${params.genre.split("%20").join(" ")} category at ReadShop. Browse a curated selection of top titles, authors, and reviews to find your next great read.`.split(
         " "
       ),
     ],
