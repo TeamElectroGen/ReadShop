fbe5ba47dbf273395b988ecde41c59741d8964ba
imranhasan2 <imranhasan030303@gmail.com>
2024-10-31 13:21:07 +0600

    updated: metaData

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,9 +1,9 @@
 import Login from "@/page/Login";
 export const generateMetadata = () => {
   return {
-    title: "Login | readShop - Join Our Book Community",
+    title: "Login -Join Our Book Community",
     description:
-      "Log in to your readShop account to continue exploring a world of books, manage your wishlist, and discover new arrivals and offers tailored just for you. Secure your reading journey today!",
+      "Log in to your ReadShop account to continue exploring a world of books, manage your wishlist, and discover new arrivals and offers tailored just for you. Secure your reading journey today!",
   };
 };
 
